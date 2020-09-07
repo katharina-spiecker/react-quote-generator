f1e74a03aa03da906a6a68a17519f03c4339b73f
Ilka no admin <ilkanoadmin@Ilkas-MBP.fritz.box>
2020-09-07 15:35:27 +0200

    Updates

---
+++ precache-manifest.ec526acdf8bdc2ab469147e6e33dd35a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a1fe392d55d9954bd1147339bd75b75",
+    "url": "/react-quote-generator/index.html"
+  },
+  {
+    "revision": "3f44b0845e12fb391d18",
+    "url": "/react-quote-generator/static/css/main.d50f4e90.chunk.css"
+  },
+  {
+    "revision": "5433e1b75d896af7eabb",
+    "url": "/react-quote-generator/static/js/2.6d0d0417.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quote-generator/static/js/2.6d0d0417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f44b0845e12fb391d18",
+    "url": "/react-quote-generator/static/js/main.62ce2614.chunk.js"
+  },
+  {
+    "revision": "2cc1455a49acdab73d08",
+    "url": "/react-quote-generator/static/js/runtime-main.211ad56b.js"
+  }
+]);
